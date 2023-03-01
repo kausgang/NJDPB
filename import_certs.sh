2ac315a4f65637a8514dece90f371e0ac59051bc
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2023-03-01 11:34:34 -0500

    Update import_certs.sh

--- import_certs.sh
+++ import_certs.sh
@@ -1,7 +1,6 @@
 # to add own certificate
 # 1. go to individual folder and take backup of keystore
-# 2. format alias name for intermediate and server certificate
-# 3. import certificates with proper alias names from the certificate location into keystore
+# 2. import certificates with proper alias names from the certificate location into keystore
 
 # to exchange certificate
 # 1. format aliasname and path of certificate. for example - box2,3,4,5,& 6's certificate will come to box1.
@@ -24,7 +23,8 @@ box6=ty-siebweb-ha-s.njes.state.nj.us
 rundate=$(date +"%d-%b-%Y")
 
 # CHANGE THIS
-main_cert_path=/home/sadmin/CA_CERTTIFICATE
+main_cert_path=/home/sadmin/CA_CERTIFICATE
+digicert_cert_path=/home/sadmin/CA_CERTIFICATE/CA_CERTS
 
 # ADD OWN CERTIFICATE
 
@@ -49,6 +49,23 @@ scp siebelkeystore.jks siebelkeystore.jks_${rundate}
 cd $main_cert_path/$box6
 scp siebelkeystore.jks siebelkeystore.jks_${rundate}
 
-# format alias name for intermediate and server certificate
-alias_intermediate=intermediate_${box1}_${rundate}
-alias_root=intermediate_${box1}_${rundate}
+# import certificates with proper alias names from the certificate location into keystore
+# ------------------------------------------------------------------------------------------
+# BOX1
+keytool -import -alias root_${box1}_${rundate} -file $digicert_cert_path/$box1/ty-sebtst4-ma-s_njes_state_nj_us.crt -keystore $main_cert_path/$box1/siebelkeystore.jks -storepass siebdev99
+keytool -import -alias intermediate_${box1}_${rundate} -file $digicert_cert_path/$box1/DigiCertCA.crt -keystore $main_cert_path/$box1/siebelkeystore.jks -storepass siebdev99
+# BOX2
+keytool -import -alias root_${box2}_${rundate} -file $digicert_cert_path/$box2/ty-sebtst5-ma-s_njes_state_nj_us.crt -keystore $main_cert_path/$box2/siebelkeystore.jks -storepass siebdev99
+keytool -import -alias intermediate_${box2}_${rundate} -file $digicert_cert_path/$box2/DigiCertCA.crt -keystore $main_cert_path/$box2/siebelkeystore.jks -storepass siebdev99
+# BOX3
+keytool -import -alias root_${box3}_${rundate} -file $digicert_cert_path/$box3/ty-sebtst6-ma-s_njes_state_nj_us.crt -keystore $main_cert_path/$box3/siebelkeystore.jks -storepass siebdev99
+keytool -import -alias intermediate_${box3}_${rundate} -file $digicert_cert_path/$box3/DigiCertCA.crt -keystore $main_cert_path/$box3/siebelkeystore.jks -storepass siebdev99
+# BOX4
+keytool -import -alias root_${box4}_${rundate} -file $digicert_cert_path/$box4/ty-sebtst7-ma-s_njes_state_nj_us.crt -keystore $main_cert_path/$box4/siebelkeystore.jks -storepass siebdev99
+keytool -import -alias intermediate_${box4}_${rundate} -file $digicert_cert_path/$box4/DigiCertCA.crt -keystore $main_cert_path/$box4/siebelkeystore.jks -storepass siebdev99
+# BOX5
+keytool -import -alias root_${box5}_${rundate} -file $digicert_cert_path/$box5/ty-sebtst8-ma-s_njes_state_nj_us.crt -keystore $main_cert_path/$box5/siebelkeystore.jks -storepass siebdev99
+keytool -import -alias intermediate_${box5}_${rundate} -file $digicert_cert_path/$box5/DigiCertCA.crt -keystore $main_cert_path/$box5/siebelkeystore.jks -storepass siebdev99
+# BOX6
+keytool -import -alias root_${box6}_${rundate} -file $digicert_cert_path/$box6/ty-siebweb-ha-s_njes_state_nj_us.crt -keystore $main_cert_path/$box6/siebelkeystore.jks -storepass siebdev99
+keytool -import -alias intermediate_${box6}_${rundate} -file $digicert_cert_path/$box6/DigiCertCA.crt -keystore $main_cert_path/$box6/siebelkeystore.jks -storepass siebdev99
