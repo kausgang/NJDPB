c4ff70d6dee2fe24e97306fdfb39edd1e2088125
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2023-03-06 08:57:01 -0500

    Create migration_exchange.sh

---
+++ migration_exchange.sh
@@ -0,0 +1,12 @@
+box1=ty-sebtst4-ma-s.njes.state.nj.us
+box2=ty-sebtst5-ma-s.njes.state.nj.us
+box3=ty-sebtst6-ma-s.njes.state.nj.us
+box4=ty-sebtst7-ma-s.njes.state.nj.us
+box5=ty-sebtst8-ma-s.njes.state.nj.us
+box6=ty-siebweb-ha-s.njes.state.nj.us
+
+rundate=$(date +"%d-%b-%Y")
+
+# CHANGE THIS
+main_cert_path=/home/typgang/CA_CERTIFICATE
+digicert_cert_path=/home/typgang/CA_CERTIFICATE/CA_CERTS
