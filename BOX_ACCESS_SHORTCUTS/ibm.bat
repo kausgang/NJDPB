6db6a427b2bd6914f7606b409fcc4e2399d520dd
Kaustav Ganguli <write.to.kaustav@gmail.com>
2024-02-27 15:48:46 -0500

    Update ibm.bat

--- BOX_ACCESS_SHORTCUTS/ibm.bat
+++ BOX_ACCESS_SHORTCUTS/ibm.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-rem start rdp.exe /f /v:10.14.103.178 /u:10.14.103.178\kaustav /p:rS_32157@nEw&Kolkata@50
+rem start rdp.exe /f /v:10.14.103.167 /u:ibmtypgang\typgang /p:rS_32157@nEw&Kolkata#1
 
 REM Creating a Newline variable (the two blank lines are required!)
 set NLM=^
@@ -9,7 +9,7 @@ set NLM=^
 set NL=^^^%NLM%%NLM%^%NLM%%NLM%
 
 
-echo %NL%"Enter uername as 10.14.103.178\kaustav and password as rS_32157@nEw&Kolkata@50 when prompted on remote server"
+echo %NL%"Enter uername as ibmtypgang\typgang and password as rS_32157@nEw&Kolkata#1 when prompted on remote server"
 pause
-cmdkey /generic:10.14.103.178 /user:10.14.103.178\kaustav /pass:"rS_32157@nEw&Kolkata@50"
-mstsc /f /v:10.14.103.178
+cmdkey /generic:10.14.103.167 /user:ibmtypgang\typgang /pass:"rS_32157@nEw&Kolkata#1"
+mstsc /f /v:10.14.103.167
